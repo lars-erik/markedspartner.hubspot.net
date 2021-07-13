8b27616b461b586a2b1cbdedf868eed2960ef480
Lars-Erik <lea@markedspartner.no>
2021-07-13 12:47:45 +0200

    Refactored merge generator a bit

--- src/hubspot-client-generator/generators/merge/generator.js
+++ src/hubspot-client-generator/generators/merge/generator.js
@@ -22,25 +22,8 @@ const transformAction = ({endpointDefinition: ed, spec}) => {
     };
 }
 
-const mergeAction = (resources) => {
-    const result = merge(resources);
-    let spec = result.output;
-    spec.info.title = 'HubSpot';
-
+const fixRefs = (spec) => {
     let fixedRefs = [];
-
-    // Object.keys(spec.paths).forEach(path => {
-    //     // console.log(path);
-    //     let ops = spec.paths[path];
-    //     Object.keys(ops).forEach(opKey => {
-    //         let op = ops[opKey];
-    //         let properName = proper(op.summary || op.operationId).replace(/ /g, '');
-    //         op.operationId = properName;
-    //         // console.log('  ' + op.operationId);
-    //     });
-    // });
-
-    delete spec.paths['/cms/v3/source-code/{environment}/validate/{path}'];
 
     Object.keys(spec.components.schemas).forEach(schemaName => {
         if (schemaName.includes(' ')) {
@@ -59,6 +42,39 @@ const mergeAction = (resources) => {
         });
         spec = JSON.parse(json);
     }
+
+    return spec;
+}
+
+const cleanOperationIds = (spec) => {
+    Object.keys(spec.paths).forEach(path => {
+        // console.log(path);
+        let ops = spec.paths[path];
+        Object.keys(ops).forEach(opKey => {
+            let op = ops[opKey];
+            let properName = proper(op.summary || op.operationId).replace(/ /g, '');
+            op.operationId = properName;
+            // console.log('  ' + op.operationId);
+        });
+    });
+
+    return spec;
+}
+
+const mergeAction = (resources) => {
+    const result = merge(resources);
+    let spec = result.output;
+    spec.info.title = 'HubSpot';
+
+    // TODO: Operation IDs suck, but there's no immediately obvious way to transform for the better.
+    // spec = cleanOperationIds(spec);
+
+    // This path has an invalid spec. 
+    // TODO: figure out why and have HS fix it.
+    delete spec.paths['/cms/v3/source-code/{environment}/validate/{path}'];
+
+    // Some schema names are invalid. We replace spaces to fix it.
+    spec = fixRefs(spec);
 
     return JSON.stringify(spec, null, '  ');
 }
